84a085d299cfbbc6a27590c04dec9172902582b8
snakoneczny <szymon.nakoneczny@gmail.com>
2015-02-26 21:47:05 +0100

    scanner init

---
+++ src/org/spl/Main.java
@@ -0,0 +1,11 @@
+package org.spl;
+
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // TODO: save result to data structure
+        Scanner.scan(new File("tests\\test_0.spl"));
+    }
+}
---
+++ src/org/spl/Scanner.java
@@ -0,0 +1,11 @@
+package org.spl;
+
+import java.io.*;
+
+public class Scanner {
+
+    public final static void scan(File spl) {
+        // TODO: add data structure as type and implement scanner
+    }
+
+}
